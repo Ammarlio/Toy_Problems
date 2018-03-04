ecf2623cb1bc59e4b1b85027c69d35674acf0943
ammarlio <ammarhalbouni@gmail.com>
2018-03-05 00:03:23 +0200

    updated

--- averageAge.js
+++ averageAge.js
@@ -1,14 +1,14 @@
 // // Write a function called  averageAge that accpets an array of objects and return the average ages for the people who are between 18 and 50
 
-//  var people = [ 
-//        {name: {first: 'Ahmad', middle: 'B.', last: 'AlAhmad'}, age: 85}, 
-//        {name: {first: 'Amer', last: 'MHD'}, age: 43}, 
-//        {name: {first: 'Aya', last: 'Sultan'}, age: 36}, 
-//        {name: {first: 'Hadeel', middle: 'E.', last: 'Lami'}, age: 44}, 
-//        {name: {first: 'Hadeel', middle: 'E.', last: 'Lami'}, age: 54}, 
-//        {name: {first: 'Lina', last: 'MHD'}, age: 14} ,
-//        {name: {first: 'Obada', last: 'Eddin'}, age: 24} 
-//  ]; 
+ var people = [ 
+       {name: {first: 'Ahmad', middle: 'B.', last: 'AlAhmad'}, age: 85}, 
+       {name: {first: 'Amer', last: 'MHD'}, age: 43}, 
+       {name: {first: 'Aya', last: 'Sultan'}, age: 36}, 
+       {name: {first: 'Hadeel', middle: 'E.', last: 'Lami'}, age: 44}, 
+       {name: {first: 'Hadeel', middle: 'E.', last: 'Lami'}, age: 54}, 
+       {name: {first: 'Lina', last: 'MHD'}, age: 14} ,
+       {name: {first: 'Obada', last: 'Eddin'}, age: 24} 
+ ]; 
 //  function averageAge(peoplea) {
 //  	var sum=0;
  	
@@ -28,33 +28,23 @@
 //  averageAge(people); // 43+36+44+24 = 36.74
 
 
- function reduce(array, f, acc) { 
-       if (acc === undefined) { 
-             acc = array[0]; 
-             array = array.slice(1); 
-       } 
-       each(array, function(element, i) { 
-             acc = f(acc, element, i); 
-       }); 
-       return acc; 
- }
-
-
-
-
-
-
-
-
-function countercor(string,char){
-
-	return reduce(string.split(""),function(acc,ele){
-		if(ele===char){
-			return acc++
-
+function averageAge(array){
+	var sum=0;
+	var counter=0;
+	for (var i = 0; i < array.length; i++) {
+		if(array[i]['age']>18 && array[i]['age']<50){
+			sum+=array[i]['age'];
+			counter++;
 		}
-	},0)
+	}
+	return sum/counter;
 }
+
+
+
+
+
+
 
 
 
