15ec5e11b5867a57b639ac34d7406f2f00c56a44
ammarlio <ammarhalbouni@gmail.com>
2018-03-04 23:46:09 +0200

    updated

--- Oop.js
+++ Oop.js
@@ -26,48 +26,48 @@
 //   }
 // }
 
-function randInt(n) {
-    return Math.floor(Math.random() * (n + 1));
-}
-
-var makeGame=function(upperbound){
-	var obj={}
-	obj.upper=upperbound;
-	obj.rand=randInt(upperbound)
-	obj.counter=0
-	obj.guessMyNumber=guessMyNumber;
-	obj.giveUp=giveUp;
-	obj.numOfGuesses=numOfGuesses;
-	return obj;
-}
 
 
-
-var guessMyNumber=function(n){
-	var x=this;
-	x.counter++;
-	   if (n > x.upper) {
-	      return "Out of bounds! Please try a number between 0 and " + x.upper + ".";
-	    } else if (n === x.rand) {
-	      return "You guessed my number!";
-	    }
-	    return "Nope! That wasn't it!";
-
-} 
-
-var giveUp=function(){
-	var x=this;
-	return x.rand;
-}
-
-var numOfGuesses=function(){
-	 var x=this;
-	return x.counter;
-}
+	var makeGame=function(upperbound){
+		var obj={}
+		obj.upper=upperbound;
+		obj.rand=randInt(obj.upper)
+		obj.counter=0	
+		obj.guessMyNumber=guessMyNumber;
+		obj.giveUp=giveUp;
+		obj.numOfGuesses=numOfGuesses;
+		return obj;
+	}
 
 
+	var randInt=function(n) {
+	    return Math.floor(Math.random() * (n + 1));
+	}
+	var guessMyNumber=function(n){
+		var x=this;
+		x.counter++;
+		   if (n > x.upper) {
+		      return "Out of bounds! Please try a number between 0 and " + x.upperbound + ".";
+		    } else if (n === x.rand) {
+		      return "You guessed my number!";
+		    }
+		    return "Nope! That wasn't it!";
 
-var Ammar=makeGame(2)
+	} 
+
+	var giveUp=function(){
+		var x=this;
+		return x.rand;
+	}
+
+	var numOfGuesses=function(){
+		 var x=this;
+		return x.counter;
+	}
+
+
+
+	var Ammar=makeGame(5)
 
 
 
